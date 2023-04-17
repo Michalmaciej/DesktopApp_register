cb9f68f25c96de4b09b2889029069606b408c249
Michał <103456890+Michalmaciej@users.noreply.github.com>
2023-04-17 23:08:47 +0200

    Add files via upload

--- register.py
+++ register.py
@@ -43,14 +43,117 @@ else:
 
 def Exit():
     root.destroy()
+
+#show image
     
+def showimage():
+    global filename
+    global img
+    filename=filedialog.askopenfilename(initialdir=os.getcwd(), title="Select image file", filetype=(("JPG File", "*.jpg"),("PNG File", "*.png"),("All Files", "*.txt")))
+    img = (Image.open(filename))
+    resized_image=img.resize(190,190)
+    photo2=ImageTk.PhotoImage(resized_image)
+    lbl.config(image=photo2)
+    lbl.image=photo2
+
+#registration NO.
+
+def registration_no():
+    file=openpyxl.load_workbook('Student_data.xlsx')
+    sheet=file.active
+    row=sheet.max_row
+
+    max_row_value=sheet.cell(row=row,column=1).value
+
+    try:
+        Registration.set(max_row_value+1)
+
+    except:
+        Registration.set("1")
+
+#clear
+
+def clear():
+    global img
+    Name.set('')
+    DOB.set('')
+    Religion.set('')
+    Skill.set('')
+    F_Name.set('')
+    M_Name.set('')
+    Father_Occupation.set('')
+    Mother_Occupation.set('')
+    Class.set("Select Class")
+
+    registration_no()
+
+    saveButton.config(state = 'normal')
+
+    img1=PhotoImage(file='images/upload.png')
+    lbl.config(image=img1)
+    lbl.image=img1
+
+    img=""
+
+#save
+
+def save():
+    R1=Registration.get()
+    N1=Name.get()
+    C1=Class.get()
+    try:
+        G1=gender
+    except:
+        messagebox.showerror("error", "Select Gender!")
+
+    D2=DOB.get()
+    D1=Date.get()
+    Re1=Religion.get()
+    S1=Skill.get()
+    fathername=F_Name.get()
+    mothername=M_Name.get()
+    F1=Father_Occupation.get()
+    M1=Mother_Occupation.get()
+
+    if N1=="" or C1=="Select Class" or D2=="" or Re1=="" or S1=="" or fathername=="" or mothername=="" or F1=="" or M1=="":
+        messagebox.showerror("error", "Few data is missing!")
+
+    else:
+        file=openpyxl.load_workbook('Student_data.xlsx')
+        sheet=file.active
+        sheet.cell(column=1,row=sheet.max_row+1,value=R1)
+        sheet.cell(column=2,row=sheet.max_row,value=N1)
+        sheet.cell(column=3,row=sheet.max_row,value=C1)
+        sheet.cell(column=4,row=sheet.max_row,value=G1)
+        sheet.cell(column=5,row=sheet.max_row,value=D2)
+        sheet.cell(column=6,row=sheet.max_row,value=D1)
+        sheet.cell(column=7,row=sheet.max_row,value=Re1)
+        sheet.cell(column=8,row=sheet.max_row,value=S1)
+        sheet.cell(column=9,row=sheet.max_row,value=fathername)
+        sheet.cell(column=10,row=sheet.max_row,value=mothername)
+        sheet.cell(column=11,row=sheet.max_row,value=F1)
+        sheet.cell(column=12,row=sheet.max_row,value=M1)
+
+        file.save(r'Student_data.xlsx')
+
+        try:
+            img.save("Student Images/"+str(R1)+".jpg")
+        except:
+            messagebox.showinfo("info","Profile Picture is not available!")
+        messagebox.showinfo("info", "Sucessfully data entered!")
+
+        clear()
+        registration_no()
+
 #gender
-    def selection():
-        value=radio.get()
-        if value==1:
-            gender="Male"
-        else:
-            gender="Female"
+
+def selection():
+    global gender
+    value=radio.get()
+    if value==1:
+        gender="Male"
+    else:
+        gender="Female"
             
 #top frame
 
@@ -60,11 +163,11 @@ Label(root,text="STUDENT REGISTRATION", width=10, height=2, bg="#c36464",fg="#ff
 #search box to update
 Search=StringVar()
 Entry(root, textvariable=Search, width=15, bd=2, font="arial 20").place(x=820,y=70)
-imageicon3=PhotoImage(file="Images/search.png")
+imageicon3=PhotoImage(file="images/search.png")
 Srch=Button(root,text="Search", compound=LEFT, image=imageicon3, width=123, bg='#68ddfa', font="arial 13 bold")
 Srch.place(x=1060,y=66)
 
-imageicon4=PhotoImage(file="Images/Layer 4.png")
+imageicon4=PhotoImage(file="images/Layer4.png")
 Update_button=Button(root, image=imageicon4, bg="#c36464")
 Update_button.place(x=110, y=64)
 
@@ -72,13 +175,13 @@ Update_button.place(x=110, y=64)
 Label(root,text="Registration No:", font="arial 13", fg=framebg, bg=background).place(x=30, y=150)
 Label(root,text="Date:", font="arial 13", fg=framebg, bg=background).place(x=500, y=150)
 
-Registration=StringVar()
+Registration=IntVar()
 Date=StringVar()
 
 reg_entry=Entry(root, textvariable=Registration, width=15, font="arial 10")
 reg_entry.place(x=160,y=150)
 
-#registration_no()
+registration_no()
 
 today=date.today()
 d1=today.strftime("%d/%m/%Y")
@@ -134,22 +237,22 @@ Label(obj2, text="Father's Name:", font="arial 13", bg=framebg, fg=framefg).plac
 Label(obj2, text="Occupation", font="arial 13", bg=framebg, fg=framefg).place(x=30, y=100)
 
 F_Name=StringVar()
-F_entry=Entry(obj,textvariable=F_Name,width=20,font="arial 10")
+F_entry=Entry(obj2,textvariable=F_Name,width=20,font="arial 10")
 F_entry.place(x=160,y=50)
 
 Father_Occupation=StringVar()
-FO_entry=Entry(obj,textvariable=Father_Occupation,width=20,font="arial 10")
+FO_entry=Entry(obj2,textvariable=Father_Occupation,width=20,font="arial 10")
 FO_entry.place(x=160,y=100)
 
 Label(obj2, text="Mother's Name:", font="arial 13", bg=framebg, fg=framefg).place(x=500, y=50)
 Label(obj2, text="Occupation", font="arial 13", bg=framebg, fg=framefg).place(x=500, y=100)
 
 M_Name=StringVar()
-M_entry=Entry(obj,textvariable=M_Name,width=20,font="arial 10")
+M_entry=Entry(obj2,textvariable=M_Name,width=20,font="arial 10")
 M_entry.place(x=630,y=50)
 
 Mother_Occupation=StringVar()
-MO_entry=Entry(obj,textvariable=Mother_Occupation,width=20,font="arial 10")
+MO_entry=Entry(obj2,textvariable=Mother_Occupation,width=20,font="arial 10")
 MO_entry.place(x=630,y=100)
 
 #image
@@ -157,18 +260,18 @@ MO_entry.place(x=630,y=100)
 f=Frame(root,bd=3,bg="black",width=200,height=200,relief=GROOVE)
 f.place(x=1000,y=150)
 
-img=PhotoImage(file="Images/upload photo.png")
+img=PhotoImage(file="images/upload.png")
 lbl=Label(f,bg="black",image=img)
 lbl.place(x=0,y=0)
 
 #button
 
-Button(root,text="Upload",width=19,height=2,font="arial 12 bold",bg="lightblue").place(x=1000,y=370)
+Button(root,text="Upload",width=19,height=2,font="arial 12 bold",bg="lightblue",command=showimage).place(x=1000,y=370)
 
-saveButton=Button(root,text="Save",width=19,height=2,font="arial 12 bold",bg="lightgreen")
+saveButton=Button(root,text="Save",width=19,height=2,font="arial 12 bold",bg="lightgreen",command=save)
 saveButton.place(x=1000,y=450)
 
-Button(root,text="Reset",width=19,height=2,font="arial 12 bold",bg="lightpink").place(x=1000,y=530)
+Button(root,text="Reset",width=19,height=2,font="arial 12 bold",bg="lightpink",command=clear).place(x=1000,y=530)
 
 Button(root,text="Exit",width=19,height=2,font="arial 12 bold",bg="grey",command=Exit).place(x=1000,y=610)
 
